91d1d2150772939559b00bbe2d2ecae32e65da4f
Emile Akbarzadeh <emile.aks@gmail.com>
2016-08-31 21:02:56 +1000

    Updating and building code

--- code/SCRUBot-attiny_v1/SCRUBot-attiny_v1.ino
+++ code/SCRUBot-attiny_v1/SCRUBot-attiny_v1.ino
@@ -1,17 +1,49 @@
 #include <TinyWireS.h>
-
 #include <Stepper.h>
 
+#define I2C_SLAVE_ADDR  0x26 
 
 //Variables
 const int stepsPerRev = 200;
 
 void setup() {
   // put your setup code here, to run once:
-
+  TinyWireS.begin(I2C_SLAVE_ADDR);
+  
+  // register the onReceive() callback function
+  TinyWireS.onReceive(receiveEvent);
+  
+  // register the onRequest() callback function
+  TinyWireS.onRequest(requestEvent);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
 }
+
+void receiveEvent(uint8_t num_bytes){
+  uint8_t dataByte = TinyWireS.receive();
+  switch(dataByte)
+  {
+    case 00000000:
+    //resets MCU
+      #define soft_reset()        \
+      do                          \
+      {                           \
+          wdt_enable(WDTO_15MS);  \
+          for(;;)                 \
+          {                       \
+          }                       \
+      } while(0)
+      break;
+      
+  }
+
+  
+}
+
+void requestEvent(){
+  
+}
+
