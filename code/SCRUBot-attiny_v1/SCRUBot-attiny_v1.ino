18026ae706d00f627a43a39540384ae6f7165beb
Emile Akbarzadeh <emile.aks@gmail.com>
2016-08-31 20:28:05 +1000

    Add code

---
+++ code/SCRUBot-attiny_v1/SCRUBot-attiny_v1.ino
@@ -0,0 +1,17 @@
+#include <TinyWireS.h>
+
+#include <Stepper.h>
+
+
+//Variables
+const int stepsPerRev = 200;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
