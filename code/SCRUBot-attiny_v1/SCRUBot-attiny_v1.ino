25dabae49b599c81a70db8d07c3d138ef25f1bfc
Emile Akbarzadeh <emile.aks@gmail.com>
2016-08-31 22:15:30 +1000

    added more cases anf functions

--- code/SCRUBot-attiny_v1/SCRUBot-attiny_v1.ino
+++ code/SCRUBot-attiny_v1/SCRUBot-attiny_v1.ino
@@ -5,6 +5,10 @@
 
 //Variables
 const int stepsPerRev = 200;
+int tnthMovesToComplete = 0;
+bool checkBuffer = false;
+//Initialisation
+Stepper stepMotor(stepsPerRev, 1, 3, 4, 5);
 
 void setup() {
   // put your setup code here, to run once:
@@ -20,43 +24,70 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
+  if(checkBuffer){
+    checkBuffer = false;
+    uint8_t typeByte = TinyWireS.receive();
+    uint8_t dataByte = TinyWireS.receive();
+    switch(dataByte)
+    {
+      case 00000000:
+      //resets MCU
+      //Bit o' C
+        #define soft_reset()        \
+        do                          \
+        {                           \
+            wdt_enable(WDTO_15MS);  \
+            for(;;)                 \
+            {                       \
+            }                       \
+        } while(0)
+        break;
+  
+        case 00000001:
+          //move instruction
+          moveFunction(dataByte);
+          break;
+  
+        case 00000010:
+          //update speed function
+          stepMotor.setSpeed(map(dataByte, 0, 255, 0, 100));
+          break;
+          
+        case 00000011:
+        //stop, sets moves to complete to zero
+          tnthMovesToComplete = 0;
+          break;
+    }
+  }
 }
 
 void receiveEvent(uint8_t num_bytes){
-  uint8_t dataByte = TinyWireS.receive();
-  switch(dataByte)
-  {
-    case 00000000:
-    //resets MCU
-    //Bit o' C
-      #define soft_reset()        \
-      do                          \
-      {                           \
-          wdt_enable(WDTO_15MS);  \
-          for(;;)                 \
-          {                       \
-          }                       \
-      } while(0)
-      break;
-
-      case 00000001:
-        //move instruction
-        
-        break;
-
-      case 00000010:
-        //update speed function
-        break;
-  }
-
-  
+  checkBuffer = true;
 }
 
 void requestEvent(){
   
 }
 
-void moveFunction(){
+void moveFunction(uint8_t tnthMoves){
+  //update moves to make
+  tnthMovesToComplete = tnthMovesToComplete + tnthMoves;
+  
+  for(tnthMovesToComplete; tnthMovesToComplete>0; tnthMovesToComplete--)
+  {
+    stepMotor.step(20);
+  }
+  
+  //send confirmation of rounds
+  sendFunc(00001001, 00000000);
   
 }
+
+void sendFunc(uint8_t typeByte, uint8_t dataByte){
+  TinyWireS.send(typeByte);
+  TinyWireS.send(dataByte);
+}
+
+
+
 
